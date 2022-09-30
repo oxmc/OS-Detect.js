95bb59fb7cb54665c1ab4572cefc0a2806f62a3d
oxmc <67136658+oxmc@users.noreply.github.com>
2022-09-30 16:58:06 -0700

    Add pollyfill for replaceAll for older browsers

--- OS-Detect.js
+++ OS-Detect.js
@@ -1,5 +1,18 @@
 /*OS-Detect.js Copyright oxmc 2021-present*/
 
+/* Polly fill for .replaceAll() */
+if (!String.prototype.replaceAll) {
+  String.prototype.replaceAll = function (str, newStr){
+    // If a regex pattern
+    if (Object.prototype.toString.call(str).toLowerCase() === '[object regexp]') {
+			return this.replace(str, newStr);
+		}
+		// If a string
+		return this.replace(new RegExp(str, 'g'), newStr);
+	};
+};
+
+/* OS-Detect Function */
 async function DetectOS(opts) {
   /*Variables*/
   var useragent = navigator.userAgent;
